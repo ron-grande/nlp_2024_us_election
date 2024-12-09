bdf32c813aa635cd492ba27734ab5465212024b9
Ron Grande <rongrande@gmail.com>
2024-12-09 12:44:51 +1000

    Multiple Changes
    1. Adjusted db file path
    2. Added condition to metadata query
    3. Added staging dir for data moving to bronze
    4. Changed metadata key name
    5. Ensured metadata_dict gets copied properly
    6. Changed filename naming convention to incl. milliseconds to ensure files get created properly

--- adhoc/gnews/bronze/midterms_to_stage.py
+++ adhoc/gnews/bronze/midterms_to_stage.py
@@ -7,12 +7,14 @@ from hashlib import sha256
 from datetime import datetime, timedelta
 
 # Collect config values from db
-db_file_path = '../../nlp_2024_us_election_db/nlp_dev'
-db_conn = sqlite3.connect(db_file_path)
+db_file_path = '../../../nlp_2024_us_election_db/nlp_dev'
+db_conn = sqlite3.connect(db_file_path) # Establish connection to db
 cursor = db_conn.cursor()
-metadata_query_results = cursor.execute("SELECT * FROM config_vw_api_metadata WHERE source_name = 'gnews'").fetchall()[0]
-metadata_query_results
-db_conn.close()
+metadata_query_results = cursor.execute( #
+    "SELECT * FROM config_vw_api_metadata WHERE 1=1  AND source_name = 'gnews' AND event_name = 'us 2022 midterm elections'"
+    ).fetchall()[0] # Query for source and event details and collect
+
+db_conn.close() # Close db connection
 
 # Constants
 GNEWS_API_KEY = os.environ['GNEWS_API_KEY']
@@ -23,7 +25,7 @@ EVENT_END_DATE = metadata_query_results[3]
 DATA_FORMAT = metadata_query_results[4]
 DATA_SOURCE_TYPE = metadata_query_results[5]
 API_URL = metadata_query_results[6]
-DATA_DIR_PATH = '../../DATA/gnews/midterms/'
+DATA_DIR_PATH = '../../../DATA/gnews/midterms/stage/'
 
 # Convert event date range to datetime
 date_range_min = datetime.strptime(EVENT_START_DATE, '%d %b %Y')
@@ -35,7 +37,7 @@ current_date = date_range_min
 
 metadata_dict = {
         'source_name': DATA_SOURCE_NAME
-        , 'event': EVENT_NAME
+        , 'event_name': EVENT_NAME
         , 'data_format': DATA_FORMAT
         , 'source_type': DATA_SOURCE_TYPE
     }
@@ -43,7 +45,7 @@ metadata_dict = {
 while current_date <= date_range_max:
     event_end_date_time = current_date.replace(hour=23, minute=59).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    api_req_dict = metadata_dict
+    api_req_dict = metadata_dict.copy()
     api_req_dict['req_url'] = f"{API_URL}&from={current_date.strftime('%Y-%m-%dT%H:%M:%SZ')}&to={event_end_date_time}&apikey={GNEWS_API_KEY}"
     api_req_dict['event_start_date'] = current_date.strftime('%Y-%m-%dT%H:%M:%SZ')
     api_req_dict['event_end_date'] = event_end_date_time
@@ -52,10 +54,13 @@ while current_date <= date_range_max:
 
     current_date += timedelta(days = 1)
 
+file_dicts = []
+
 # Request data from gnews
 for i in range(len(req_dicts)):
     req_dict = req_dicts[i]
 
+    print(f"{i + 1} GET {req_dict['req_url']}")
     r = requests.get(req_dict['req_url'])
 
     if not r.ok:
@@ -68,29 +73,28 @@ for i in range(len(req_dicts)):
     json_payload = r.json()
     req_dict['article_count'] = len(json_payload['articles'])
 
-    req_dicts[i] = {
+    file_dicts.append({
         'gnews_api_data': json_payload
         , 'metadata': req_dict
-    }
+    })
 
-    url_api_key_param_xd = req_dicts[i]['metadata']['req_url'].replace(f'apikey={GNEWS_API_KEY}', 'apikey=xxx')
-    req_dicts[i]['metadata']['req_url'] = url_api_key_param_xd
-    record_id_str = req_dicts[i]['metadata']['source_name'] + req_dicts[i]['metadata']['event'] + req_dicts[i]['metadata']['event_start_date'] + req_dicts[i]['metadata']['event_end_date']
+    url_api_key_param_xd = file_dicts[i]['metadata']['req_url'].replace(f'apikey={GNEWS_API_KEY}', 'apikey=xxx')
+    file_dicts[i]['metadata']['req_url'] = url_api_key_param_xd
+    record_id_str = file_dicts[i]['metadata']['source_name'] + file_dicts[i]['metadata']['event_name'] + file_dicts[i]['metadata']['event_start_date'] + file_dicts[i]['metadata']['event_end_date']
     record_id_hex = sha256(bytes(record_id_str, 'utf-8')).hexdigest()
 
-    req_dicts[i]['metadata']['bronze_record_id'] = record_id_hex
-    req_dicts[i]['metadata']['bln_isProcessedToSilver'] = 0
+    file_dicts[i]['metadata']['bronze_record_id'] = record_id_hex
+    file_dicts[i]['metadata']['bln_isProcessedToSilver'] = 0
     
-    for j in range(len(req_dicts[i]['gnews_api_data']['articles'])):
-        article_id_str = record_id_hex + req_dicts[i]['gnews_api_data']['articles'][j]['title'] + req_dicts[i]['gnews_api_data']['articles'][j]['source']['name'] + req_dicts[i]['gnews_api_data']['articles'][j]['publishedAt']
+    for j in range(len(file_dicts[i]['gnews_api_data']['articles'])):
+        article_id_str = record_id_hex + file_dicts[i]['gnews_api_data']['articles'][j]['title'] + file_dicts[i]['gnews_api_data']['articles'][j]['source']['name'] + file_dicts[i]['gnews_api_data']['articles'][j]['publishedAt']
         article_id_hex = sha256(bytes(article_id_str, 'utf-8')).hexdigest()
 
-        req_dicts[i]['gnews_api_data']['articles'][j]['bronze_article_id'] = article_id_hex
-    
+        file_dicts[i]['gnews_api_data']['articles'][j]['bronze_article_id'] = article_id_hex
 
-for record in req_dicts:
+for record in file_dicts:
     dtm = datetime.now()
-    dtm_str = dtm.strftime('%Y%m%d%H%M%S')
+    dtm_str = dtm.strftime('%Y%m%d%H%M%S') + f"{dtm.microsecond // 1000:03d}"
 
     file_name = f"{dtm_str}.{DATA_FORMAT}"
 
