9c83a65cabf6ba8002c815ba677fff6e315077db
Ron Grande <rongrande@gmail.com>
2024-12-09 14:07:21 +1000

    Multiple Changes Made

--- adhoc/gnews/bronze/data_to_bronze.py
+++ adhoc/gnews/bronze/data_to_bronze.py
@@ -12,9 +12,9 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 # gdrive credentials file
 SERVICE_ACCOUNT_FILE = '../../../token.json'
 # Bronze layer dir 
-GNEWS_BRONZE_FOLDER_ID = environ['GNEWS_BRONZE_FOLDER_ID']
+GNEWS_BRONZE_MIDTERMS_FOLDER_ID = environ['GNEWS_BRONZE_MIDTERMS_FOLDER_ID']
 
-MIDTERMS_FILES_PATH = '../../../DATA/gnews/midterms/bronze/'
+MIDTERMS_FILES_PATH = '../../../DATA/gnews/midterms/stage/'
 
 # Authenticate using the service account credentials
 credentials = service_account.Credentials.from_service_account_file(
@@ -33,7 +33,7 @@ for file_path in file_paths:
         file_dict['metadata']['dtm_DatetimeUploaded'] = datetime.now(timezone(timedelta(hours=10))).strftime("%Y-%m-%dT%H:%M:%S%z") 
 
         # Create an in-memory stream
-        fh = io.BytesIO(bytes(json.dumps(file_dict['gnews_api_data']), 'utf-8'))
+        fh = io.BytesIO(bytes(json.dumps(file_dict), 'utf-8'))
 
         dtm = datetime.now()
         dtm_str = dtm.strftime('%Y%m%d%H%M%S')
@@ -42,7 +42,7 @@ for file_path in file_paths:
 
         file_metadata = {
             'name' :file_name
-            , 'parents': [GNEWS_BRONZE_FOLDER_ID]
+            , 'parents': [GNEWS_BRONZE_MIDTERMS_FOLDER_ID]
             # , 'properties': file_dict['metadata']
         }
 
